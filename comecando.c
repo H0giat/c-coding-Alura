2ab98bcb7810e72d9b84ce848838f1045e25ed6e
Thiago <thiagocarvalhal77@gmail.com>
2023-02-18 11:40:23 -0300

    Lendo valores do usuário

--- comecando.c
+++ comecando.c
@@ -13,6 +13,11 @@ int main(){
     printf("Tetando quebra\nde linha\n");
 
     int numero = 7;
+    int chute;
 
-    printf("Adoro o numero %d", numero);
+    // printf("Adoro o numero %d", numero);
+
+    printf("Qual o numero que pensei?: ");
+    scanf("%d", &chute);    // é obrigatório aquele '&' para ler entrada de usuários | scanf é para ler do teclado
+    printf("Seu chute foi: %d", chute);
 }
