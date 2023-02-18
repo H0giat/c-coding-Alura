8db76ca2a38d3ade0a382098f8f09f6cc80a7bc3
Thiago <thiagocarvalhal77@gmail.com>
2023-02-18 11:05:43 -0300

    Como fazer comentários

--- comecando.c
+++ comecando.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
+
+    // assim que faz um comentário de linha
+
+    /*
+        assim que faz um comentário de bloco,
+        caso precise
+    */
+
     printf("Hello world\n");
     printf("Tetando quebra\nde linha");
 }
