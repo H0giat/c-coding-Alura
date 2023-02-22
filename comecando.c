8990b2506ad061dcc02c2968051b6a4a07e58fa1
Thiago <thiagocarvalhal77@gmail.com>
2023-02-22 15:36:40 -0300

    Limite de tentativas

--- comecando.c
+++ comecando.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 
 int main(){
-
-    // assim que faz um comentário de linha
-
-    /*
-        assim que faz um comentário de bloco,
-        caso precise
-    */
-
-    printf("Hello world\n");
-    printf("Tetando quebra\nde linha\n");
-
     int numero = 7;
     int chute;
 
-    // printf("Adoro o numero %d", numero);
+    printf("Bem-vindo ao jogo de adivinhacao\n");
 
-    printf("Digite seu chute: ");
-    scanf("%d", &chute);
+    for(int i = 1; i <= 3; i++){
+        printf("Tentativa %d de 3\n", i);
+        printf("Digite seu chute: ");
+        scanf("%d", &chute);
 
-    if(chute == numero){
-        printf("Parabens, voce acertou o numero: %d!", numero);
-    }else{
-        printf("Nao foi desssa vez, mas continue tentando!!!");
+        if(chute == numero){
+            printf("Parabens, voce acertou o numero: %d!", numero);
+            break;
+        }else{
+            if(chute < numero){
+                printf("Tente mais alto\n");
+            }else{
+                printf("Tente mais baixo\n");
+            }
+        }
     }
 }
