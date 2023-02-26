79031236a79e724ef99f9eef21d74c3616ca6aca
Thiago <thiagocarvalhal77@gmail.com>
2023-02-26 14:05:54 -0300

    Pequenas mudanças, para melhor leitura

--- comecando.c
+++ comecando.c
@@ -3,15 +3,17 @@
 int main(){
     int numero = 7;
     int chute;
+    int acertou = (chute == numero);
+    int numero_tentativas = 3;
 
     printf("Bem-vindo ao jogo de adivinhacao\n");
 
-    for(int i = 1; i <= 3; i++){
+    for(int i = 1; i <= numero_tentativas; i++){
         printf("Tentativa %d de 3\n", i);
         printf("Digite seu chute: ");
         scanf("%d", &chute);
 
-        if(chute == numero){
+        if(acertou){
             printf("Parabens, voce acertou o numero: %d!", numero);
             break;
         }else{
