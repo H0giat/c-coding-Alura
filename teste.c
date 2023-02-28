bd4d1582de14b8f4da9b3a043a7a80702f72cefe
Thiago <thiagocarvalhal77@gmail.com>
2023-02-28 11:36:18 -0300

    Somando todos os números de 1 a 100

---
+++ teste.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+int main(){
+    int num = 0;
+    int soma = 0;
+
+    while(num < 100){
+        num++;
+        soma += num;
+    }
+    printf("%d\n", soma);
+/////////////////////////////////////////////////////////////
+    
+#include <stdio.h>
+
+int main() {
+    int soma = 0;
+    for(int i = 1; i <= 100; i++) {
+        soma = soma + i;
+    }
+
+    printf("A soma eh %d", soma);
+}
+}
+
+*/
+
